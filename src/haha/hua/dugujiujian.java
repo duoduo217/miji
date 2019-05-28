d97b1305d8f92a82a86cb7b2caea3b4e679c2a7e
zhaohua1 <zhaohua217>
2019-05-28 14:16:32 +0800

    "添加的剑法"

--- src/haha/hua/dugujiujian.java
+++ src/haha/hua/dugujiujian.java
@@ -5,6 +5,8 @@ public class dugujiujian {
 		System.out.println("1111");
 		System.out.println("2222");
 		System.out.println("3333");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
 		
 	}
 }
